8e439b27594b9645989cf4baa940dd319a803e6e
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-22 11:25:17 +0400

    задача34

---
+++ задача34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа задает массив случайных положительных трехзначных чисел, показывает количество четных чисел в массиве
+int[] generateArray(int Length = 10, int min = 100, int max = 999)
+{
+    int[] result = new int[Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+int[] array = generateArray();
+Console.WriteLine(string.Join(", ", array));
+
+void Odd (int[] array)
+{
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine("Количество четных чисел в массиве: " + result);
+}
+Odd(array);
